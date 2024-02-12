bf821af128a1a01b0dd1bfbfb4ccfd76a63eb154
Abraham <abrahamrichard422@gmail.com>
2024-02-12 19:49:18 +0530

    Deployment

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 // backend/index.js
 require("dotenv").config();
+const path = require("path");
 const express = require('express');
 const cors = require("cors");
 const rootRouter = require("./routes/index");
@@ -10,6 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
   
 app.listen(process.env.PORT);
