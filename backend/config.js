148aa2cc5bdf095a67eb1b4649d1c21c113b2114
Abraham <abrahamrichard422@gmail.com>
2024-02-14 23:22:05 +0530

    Deployment

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-//backend/config.js
-module.exports = {
-	JWT_SECRET: "your-jwt-secret"
-}
+// //backend/config.js
+// module.exports = {
+// 	JWT_SECRET: "0e0a22d5c7c6ae862cf6558ed1be647ee2c57eef93900d0d30130e44e728c434"
+// }
