8ff08673e254b2c157a3a0956f61bfec46966c93
Abraham <abrahamrichard422@gmail.com>
2024-02-15 22:12:44 +0530

    Deployment bug fixed

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 // backend/index.js
-const dotenv = require("dotenv").config();
 const path = require("path");
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({path: __dirname+'/.env'});
+  }
 const express = require('express');
 const cors = require("cors");
 const rootRouter = require("./routes/index");
@@ -11,10 +13,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
-app.use(express.static(path.join(__dirname,"/frontend/dist")))
-
-app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
-})
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend', 'build')));
+    app.get('/*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
+    })
+  }
   
 app.listen(process.env.PORT);
